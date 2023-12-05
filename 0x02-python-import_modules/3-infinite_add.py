3ab394f7ebc708dca8e0c0a24869f00b9ccab873
NombuHatta <buli.hatta@gmail.com>
2023-12-05 11:23:17 -0800

    Print result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+def add_arguments(arguments):
+    result = sum(int(arg) for arg in arguments)
+    return result
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+
+    if not arguments:
+        print(0)
+    else:
+        result = add_arguments(arguments)
+        print(result)
